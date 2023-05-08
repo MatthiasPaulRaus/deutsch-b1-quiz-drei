6588e0f4caf9c67b4cb71a41a591dc097e20ddd0
matthias paul raus <81903294+MatthiasPaulRaus@users.noreply.github.com>
2023-05-08 15:47:20 +0200

    first commit

---
+++ deutsch-b1-quiz-drei/src/QuestionsList.js
@@ -0,0 +1,221 @@
+const QuestionsList = [
+    {
+        question:"Das Wetter war so schlecht.",
+        answersList: [
+            {answer:"Aus diesem Grund haben wir das Turnier abgesagt.", isCorrect:true},
+            {answer:"Daher haben wir nicht abgesagt.", isCorrect:false},
+            {answer:"Deshalb haben wir teilgenommen.", isCorrect:false},
+        ]
+    },{
+        question:"Wegen seines Dialekts",
+        answersList: [
+            {answer:"konnte man ihn gut verstehen.", isCorrect:false},
+            {answer:"konnte man ihn oft nicht verstehen.", isCorrect:true},
+            {answer:"konnte er nichts verstehen.", isCorrect:false},
+        ]
+    },{
+        question:"Kurz vor dem Spiel hat es ein Gewitter gegeben.",
+        answersList: [
+            {answer:"Deshalb haben wir draußen gespielt.", isCorrect:false},
+            {answer:"Aus diesem Grund konnten wir im Park spielen.", isCorrect:false},
+            {answer:"Daher konnte man nicht im Freien spielen.", isCorrect:true},
+        ]
+    },{
+        question:"Der Beamte konnte mich kaum verstehen.",
+        answersList: [
+            {answer:"Denn er sprach nur ein bisschen Englisch.", isCorrect:true},
+            {answer:"Daher konnte ich nichts verstehen.", isCorrect:false},
+            {answer:"Deshalb habe ich nichts verstanden.", isCorrect:false},
+        ]
+    },{
+        question:"Das habe ich nicht verstanden.",
+        answersList: [
+            {answer:"Bitte nicht wiederholen!", isCorrect:false},
+            {answer:"Bitte wiederholen Sie das!", isCorrect:true},
+            {answer:"Verstehen Sie mich?", isCorrect:false},
+        ]
+    },{
+        question:"Sie sind sehr sportlich.",
+        answersList: [
+            {answer:"Den Sprachkurs kann ich Ihnen empfehlen.", isCorrect:false},
+            {answer:"Ein Tanzkurs ist dir am liebsten.", isCorrect:false},
+            {answer:"Der Kletterkurs wäre für Sie geeignet.", isCorrect:true},
+        ]
+    },{
+        question:"Haben Sie gute Sprachkenntnisse?",
+        answersList: [
+            {answer:"Ich spreche sowohl Deutsch als auch Englisch.", isCorrect:true},
+            {answer:"Ich spreche weder Italienisch noch Spanisch.", isCorrect:false},
+            {answer:"Sie sprechen nicht nur Französisch sondern auch Polnisch.", isCorrect:false},
+        ]
+    },{
+        question:"Wann haben Sie Ihre Ausbildung abgeschlossen?",
+        answersList: [
+            {answer:"In drei Jahren.", isCorrect:false},
+            {answer:"Vor zwei Jahren.", isCorrect:true},
+            {answer:"Bis zum nächsten Jahr.", isCorrect:false},
+        ]
+    },{
+        question:"An der Volkshochschule",
+        answersList: [
+            {answer:"können Sie Tipps geben.", isCorrect:false},
+            {answer:"können Sie Autofahren lernen.", isCorrect:false},
+            {answer:"können Sie viele Kurse besuchen.", isCorrect:true},
+        ]
+    },{
+        question:"Weiterbildungsangebote finden Sie",
+        answersList: [
+            {answer:"an der Volkshochschule.", isCorrect:true},
+            {answer:"in Ihrer Schule.", isCorrect:false},
+            {answer:"während der Ausbildung.", isCorrect:false},
+        ]
+    },
+    
+    {
+        question:"Unsere Eltern haben immer alles erledigt.",
+        answersList: [
+            {answer:"Wir brauchten uns um nichts zu kümmern.", isCorrect:true},
+            {answer:"Wir haben alles gemacht.", isCorrect:false},
+            {answer:"Wir haben uns nicht darum gekümmert.", isCorrect:false},
+        ]
+    },{
+        question:"Was mussten Sie als Kind zuhause machen.",
+        answersList: [
+            {answer:"Ich ging gern draußen spielen.", isCorrect:false},
+            {answer:"Wir mussten unsere Zimmer aufräumen.", isCorrect:true},
+            {answer:"Ich musste mich um meine Weiterbildung kümmern.", isCorrect:false},
+        ]
+    },{
+        question:"Bei uns zuhause gab es immer viel zu tun.",
+        answersList: [
+            {answer:"Aus diesem Grund habe ich mich darum gekümmert.", isCorrect:false},
+            {answer:"Daher haben wir den Nachbarn geholfen.", isCorrect:false},
+            {answer:"Deshalb mussten alle im Haushalt mithelfen.", isCorrect:true},
+        ]
+    },{
+        question:"Damals war es nicht möglich ",
+        answersList: [
+            {answer:"ins Ausland zu reisen.", isCorrect:true},
+            {answer:"eine Sprache zu lernen.", isCorrect:false},
+            {answer:"eine eigene Meinung zu haben.", isCorrect:false},
+        ]
+    },{
+        question:"Was halten Sie von diesem Künstler?",
+        answersList: [
+            {answer:"Er wird mir sehr gefallen.", isCorrect:false},
+            {answer:"Er hat mich nicht beeindruckt.", isCorrect:true},
+            {answer:"Ich finde es sehr angenehm.", isCorrect:false},
+        ]
+    },{
+        question:"Erzählen Sie mir etwas aus Ihrer Schulzeit.",
+        answersList: [
+            {answer:"Ich kann mich an ihn erinnern.", isCorrect:false},
+            {answer:"Ich kann es mir kaum vorstellen.", isCorrect:false},
+            {answer:"Es fällt mir schwer, mich daran zu erinnern.", isCorrect:true},
+        ]
+    },{
+        question:"Was machten Sie nach dem Studium.",
+        answersList: [
+            {answer:"Zuerst machte ich eine Reise ins Ausland.", isCorrect:true},
+            {answer:"Danach machte ich das Abitur.", isCorrect:false},
+            {answer:"Davor machte ich den Schulabschluss.", isCorrect:false},
+        ]
+    },{
+        question:"Schon während meiner Ausbildung",
+        answersList: [
+            {answer:"kümmerte ich mich um meine Aufgaben.", isCorrect:false},
+            {answer:"hatte ich ein großes Interesse an Fremdsprachen.", isCorrect:true},
+            {answer:"konnte ich mich nicht damit beschäftigen.", isCorrect:false},
+        ]
+    },{
+        question:"Wie geht es Ihnen heute?",
+        answersList: [
+            {answer:"Es geht ihnen nicht so gut.", isCorrect:false},
+            {answer:"Hoffentlich geht es ihm bald wieder besser.", isCorrect:false},
+            {answer:"Danke, es geht mir schon wieder besser.", isCorrect:true},
+        ]
+    },{
+        question:"Es hat ziemlich viel geregnet.",
+        answersList: [
+            {answer:"Deshalb müssen wir die Gartenparty absagen.", isCorrect:true},
+            {answer:"Deshalb kann die Party im Freien stattfinden.", isCorrect:false},
+            {answer:"Aus diesem Grund können wir nicht drinnen feiern.", isCorrect:false},
+        ]
+    },
+    
+
+    {
+        question:"Früher war alles besser!",
+        answersList: [
+            {answer:"Nein, ich bin nicht deiner Meinung.", isCorrect:true},
+            {answer:"Ja, früher warst du besser.", isCorrect:false},
+            {answer:"Ja, heutzutage ist alles besser.", isCorrect:false},
+        ]
+    },{
+        question:"Je mehr ich lernte, desto besser ",
+        answersList: [
+            {answer:"kann ich es machen.", isCorrect:false},
+            {answer:"konnte ich die Leute verstehen.", isCorrect:true},
+            {answer:"wurden seine Sprachkenntnisse.", isCorrect:false},
+        ]
+    },{
+        question:"Immer mehr Menschen engagieren sich",
+        answersList: [
+            {answer:"gegen die Mitbewohner.", isCorrect:false},
+            {answer:"für einen Verein.", isCorrect:false},
+            {answer:"für den Umweltschutz.", isCorrect:true},
+        ]
+    },{
+        question:"Ich würde gern wissen,",
+        answersList: [
+            {answer:"wer sich um die Übernachtung kümmert.", isCorrect:true},
+            {answer:"wo ist die Unterkunft?", isCorrect:false},
+            {answer:"ob wir ein Hotel suchten.", isCorrect:false},
+        ]
+    },{
+        question:"Was würden Sie in dieser Situation machen?",
+        answersList: [
+            {answer:"Ich kann mich nicht daran erinnern.", isCorrect:false},
+            {answer:"Ich konnte es mir nicht vorstellen.", isCorrect:false},
+            {answer:"Ich würde mich sofort darum kümmern.", isCorrect:true},
+        ]
+    },{
+        question:"Mein erster Eindruck war sehr positiv.",
+        answersList: [
+            {answer:"Ich würde gerne mehr über die Firma erfahren.", isCorrect:true},
+            {answer:"Ich hätte es mir denken können.", isCorrect:false},
+            {answer:"Das Gesprächsklima war ziemlich schlecht.", isCorrect:false},
+        ]
+    },{
+        question:"Bitte schicken sie die Bewerbungsunterlagen",
+        answersList: [
+            {answer:"an Ihren Personalleiter.", isCorrect:false},
+            {answer:"an unsere Personalabteilung.", isCorrect:true},
+            {answer:"an den Kundendienst.", isCorrect:false},
+        ]
+    },{
+        question:"In welchen Bereichen haben Sie Erfahrung gesammelt?",
+        answersList: [
+            {answer:"Ich interessiere mich für alle Bereiche.", isCorrect:false},
+            {answer:"Der Kundendienst war eine schlechte Erfahrung.", isCorrect:false},
+            {answer:"In der Finanzabteilung und in der Kundenbetreuung.", isCorrect:true},
+        ]
+    },{
+        question:"Meine Eltern waren nicht sehr streng.",
+        answersList: [
+            {answer:"Daher konnten wir lange ausgehen.", isCorrect:true},
+            {answer:"Deshalb mussten wir immer um 20 Uhr ins Bett gehen.", isCorrect:false},
+            {answer:"Deshalb haben sie sich um alles gekümmert.", isCorrect:false},
+        ]
+    },{
+        question:"Wie gefällt Ihnen Ihr neuer Arbeitsplatz?",
+        answersList: [
+            {answer:"Ihre Arbeit finde ich sehr interessant.", isCorrect:false},
+            {answer:"Meine Arbeit würde Ihnen gefallen.", isCorrect:false},
+            {answer:"Danke, ich bin sehr zufrieden damit.", isCorrect:true},
+        ]
+    },
+
+].sort(() => Math.random() - 0.5);
+
+export default QuestionsList;
